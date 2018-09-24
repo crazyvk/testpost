6439495d1a6290a9de4d34cbbcf91947caea87e6
Vikram Biwal <crazyvk56@gmail.com>
2018-09-24 19:58:18 +0530

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $helper = $fb->getRedirectLoginHelper();
 
 $redirectURL   = 'https://testpostvk.herokuapp.com/show.php';
 
-$permissions = ['manage_pages','publish_actions','publish_pages'];
+$permissions = ['manage_pages','publish_pages'];
 $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
 
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
