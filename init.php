08d738730715d92e6cfa1c5892ae7c4867402ae7
Vikram Biwal <crazyvk56@gmail.com>
2018-09-24 19:12:58 +0530

    Update

---
+++ init.php
@@ -0,0 +1,20 @@
+<?php
+
+
+session_start();
+
+require_once('Facebook/autoload.php');
+
+// $fb = new Facebook\Facebook([
+// 'app_id' => 'APP_ID',
+// 'app_secret' => 'APP_SECRET',
+// 'default_graph_version' => 'v2.9',
+// ]);
+
+$fb = new Facebook\Facebook([
+	'app_id' => '1166367610178804',
+	'app_secret' => '4abfedfd4d1ca06939a1ec14a2521f04',
+	'default_graph_version' => 'v2.2',
+   ]);
+
+?>
--- show.php
+++ show.php
@@ -1,7 +1,51 @@
 <?php
 
-print_r($_POST);
-print_r($_GET);
+include('init.php');
+
+$helper = $fb->getRedirectLoginHelper();
+$_SESSION['FBRLH_state']=$_GET['state'];
+
+try {
+
+  $accessToken = $helper->getAccessToken();
+
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+
+}
+
+if (! isset($accessToken)) {
+  echo 'No OAuth data could be obtained from the signed request. User has not authorized your app yet.';
+  exit;
+}
+
+try {
+
+  $response = $fb->get('me/accounts', $accessToken->getValue());
+  $response = $response->getDecodedBody();
+
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+
+}
+
+echo "<pre>";
+print_r($response);
+echo "</pre>";
 
 
 ?>
