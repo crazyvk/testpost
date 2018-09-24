22105553b7051003245d2390b4560a18c365d55b
Vikram Biwal <crazyvk56@gmail.com>
2018-09-24 19:43:17 +0530

    Url update

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ include('init.php');
 
 $helper = $fb->getRedirectLoginHelper();
 
+$redirectURL   = 'https://testpostvk.herokuapp.com/show.php';
+
 $permissions = ['manage_pages','publish_actions','publish_pages'];
-$loginUrl = $helper->getLoginUrl('YOUR_WEBSITE_URL/fb-callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
 
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 
