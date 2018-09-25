c0a88c0a984cc770e3a4aa11934d90e14bf8936e
Vikram Biwal <crazyvk56@gmail.com>
2018-09-25 19:36:39 +0530

    Update

--- main.php
+++ main.php
@@ -5,8 +5,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-	require "fbsdk/src/Facebook/autoload.php";
+require_once(__DIR__.'/src/Facebook/autoload.php');
 	session_start();
+
 	$fb = new Facebook\Facebook([
 	  'app_id'                => '1166367610178804',
 	  'app_secret'            => '4abfedfd4d1ca06939a1ec14a2521f04',
