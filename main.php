36da069e94ec4b4131586c2b11d770b5e4a356aa
crazyvk <crazyvk56@gmail.com>
2018-09-30 19:37:11 +0530

    Update main.php

--- main.php
+++ main.php
@@ -11,6 +11,6 @@ require_once(__DIR__.'/src/Facebook/autoload.php');
 	$fb = new Facebook\Facebook([
 	  'app_id'                => '1166367610178804',
 	  'app_secret'            => '4abfedfd4d1ca06939a1ec14a2521f04',
-	  'default_graph_version' => 'v3.0',
+	  'default_graph_version' => 'v3.1',
 	]);
 ?>
